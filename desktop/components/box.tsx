6f0be75ae8234cca62723cd7c67ec071544f766a
stuart <stuart.a.creed@gmail.com>
2023-11-12 11:30:36 +0000

    Next js component tinkerings

--- desktop/components/box.tsx
+++ desktop/components/box.tsx
@@ -1,9 +1,12 @@
 "use client"
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Box() {
     const [customer, setCustomer] = useState('Stuart Creed')
+    useEffect(() => {
+        console.log('mounted')
+    }, [customer]);
     return (
         <>
             <div className="bg-blue-800 text-white w-[400px] h-[400px]">Hello</div>
